fb5e98bbd47d7687243616f7d7479b17ac314a94
alevko55555 <alevko55555@gmail.com>
2019-10-19 11:09:32 +0300

    Scripted auto-commit on change (2019-10-19 11:09:32) by gitwatch.sh

--- src/main/java/lab2/Reducer.java
+++ src/main/java/lab2/Reducer.java
@@ -10,5 +10,9 @@ public class Reducer extends Reducer<TextPair, Text, Text, Text> {
     @Override
     protected void reduce( TextPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
+        String characteristic = iter.next().toString();
+        if(!iter.hasNext()) {
+            return;
+        }
     }
 }
