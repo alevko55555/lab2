1cca16887e5256bb39eee9219399839af8500488
alevko55555 <alevko55555@gmail.com>
2019-10-19 11:20:54 +0300

    Scripted auto-commit on change (2019-10-19 11:20:54) by gitwatch.sh

--- src/main/java/lab2/JoinAirportFlight.java
+++ src/main/java/lab2/JoinAirportFlight.java
@@ -1,13 +1,16 @@
 package lab2;
 
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 
 public class JoinAirportFlight {
     public static void main(String args[]) throws Exception {
         Job job = Job.getInstance();
         job.setJarByClass(JoinAirportFlight.class);
         job.setJobName("JoinAirportFlight");
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, MapperAirport.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, MapperFlight.class);
     }
 }
