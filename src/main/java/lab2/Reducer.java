e63d637423178298f839655ffe9c190c15f80462
alevko55555 <alevko55555@gmail.com>
2019-10-19 11:07:09 +0300

    Scripted auto-commit on change (2019-10-19 11:07:09) by gitwatch.sh

--- src/main/java/lab2/Reducer.java
+++ src/main/java/lab2/Reducer.java
@@ -4,10 +4,11 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer.Context;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class Reducer extends Reducer<TextPair, Text, Text, Text> {
     @Override
     protected void reduce( TextPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-
+        Iterator<Text> iter = values.iterator();
     }
 }
