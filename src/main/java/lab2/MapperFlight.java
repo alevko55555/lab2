281a7626f3232dfb885321e4b9c7403893a7cda2
alevko55555 <alevko55555@gmail.com>
2019-10-19 10:54:16 +0300

    Scripted auto-commit on change (2019-10-19 10:54:16) by gitwatch.sh

--- src/main/java/lab2/MapperFlight.java
+++ src/main/java/lab2/MapperFlight.java
@@ -1,6 +1,9 @@
 package lab2;
 
-public class MapperFlight extends Mapper<, Text, , Text> {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+public class MapperFlight extends Mapper<LongWritabler, Text, , Text> {
     @Override
-    
+
 }
