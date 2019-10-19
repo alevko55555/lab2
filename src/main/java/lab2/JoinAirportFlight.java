a3063a3cdd4486a8f1f8d0bd8aa004a92201dde1
alevko55555 <alevko55555@gmail.com>
2019-10-19 11:21:39 +0300

    Scripted auto-commit on change (2019-10-19 11:21:39) by gitwatch.sh

--- src/main/java/lab2/JoinAirportFlight.java
+++ src/main/java/lab2/JoinAirportFlight.java
@@ -12,5 +12,16 @@ public class JoinAirportFlight {
         job.setJobName("JoinAirportFlight");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, MapperAirport.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, MapperFlight.class);
+
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setReducerClass(JoinReducer.class);
+        job.setMapOutputKeyClass(TextPair.class);
+        
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
