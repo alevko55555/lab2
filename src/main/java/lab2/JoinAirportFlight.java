43b353cf838e4a4cb3b71c4334ebcc92ef4747ff
alevko55555 <alevko55555@gmail.com>
2019-10-19 11:16:11 +0300

    Scripted auto-commit on change (2019-10-19 11:16:11) by gitwatch.sh

--- src/main/java/lab2/JoinAirportFlight.java
+++ src/main/java/lab2/JoinAirportFlight.java
@@ -1,7 +1,7 @@
 package lab2;
 
 public class JoinAirportFlight {
-    public static void main(String args[]){
+    public static void main(String args[]) throws Exception {
         int k = 1;
     }
 }
