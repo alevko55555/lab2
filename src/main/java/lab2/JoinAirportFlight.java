13360f71d0c719912cd12d863dd877e1d97388e5
alevko55555 <alevko55555@gmail.com>
2019-10-19 11:17:49 +0300

    Scripted auto-commit on change (2019-10-19 11:17:49) by gitwatch.sh

--- src/main/java/lab2/JoinAirportFlight.java
+++ src/main/java/lab2/JoinAirportFlight.java
@@ -5,6 +5,6 @@ import org.apache.hadoop.mapreduce.Job;
 public class JoinAirportFlight {
     public static void main(String args[]) throws Exception {
         Job job = Job.getInstance();
-        
+        job.setJarByClass(JoinAirportFlight.class);
     }
 }
