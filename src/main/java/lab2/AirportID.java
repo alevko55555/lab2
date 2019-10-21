438f49b611941673b034a1d010e281302dc2309b
alevko55555 <alevko55555@gmail.com>
2019-10-21 14:24:24 +0300

    Scripted auto-commit on change (2019-10-21 14:24:24) by gitwatch.sh

--- src/main/java/lab2/AirportID.java
+++ src/main/java/lab2/AirportID.java
@@ -6,5 +6,15 @@ import org.apache.hadoop.io.WritableComparable;
 
 public class AirportID implements WritableComparable<AirportID> {
     private LongWritable value;
-    private IntWritable
+    private IntWritable id;
+
+    public AirportID() {
+        value = new LongWritable();
+        id = new IntWritable();
+    }
+
+    public AirportID(long value, int id) {
+        this.value = new LongWritable(value);
+        this.id = new IntWritable(id);
+    }
 }
