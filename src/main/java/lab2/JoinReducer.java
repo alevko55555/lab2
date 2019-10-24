07295ce5377963d04e6d448358fe6febc38e4171
alevko55555 <alevko55555@gmail.com>
2019-10-24 19:11:48 +0300

    Scripted auto-commit on change (2019-10-24 19:11:48) by gitwatch.sh

--- src/main/java/lab2/JoinReducer.java
+++ src/main/java/lab2/JoinReducer.java
@@ -1,12 +1,12 @@
 package lab2;
 
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 import java.util.Iterator;
 
-public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
+public class JoinReducer extends Reducer<AirportID, Text, Text, Text> {
     @Override
     protected void reduce( AirportID key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
