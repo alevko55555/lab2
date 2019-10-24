55b9376d44b5bf23e209eecc57cc5585d14d14a7
alevko55555 <alevko55555@gmail.com>
2019-10-24 19:11:13 +0300

    Scripted auto-commit on change (2019-10-24 19:11:13) by gitwatch.sh

--- src/main/java/lab2/JoinReducer.java
+++ src/main/java/lab2/JoinReducer.java
@@ -8,7 +8,7 @@ import java.util.Iterator;
 
 public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
     @Override
-    protected void reduce( TextPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+    protected void reduce( AirportID key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         String characteristic = iter.next().toString();
         if(!iter.hasNext()) {
