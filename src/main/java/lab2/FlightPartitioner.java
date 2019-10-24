a917222189d6042c8883436e8aed2759c355832e
alevko55555 <alevko55555@gmail.com>
2019-10-24 18:50:43 +0300

    Scripted auto-commit on change (2019-10-24 18:50:43) by gitwatch.sh

--- src/main/java/lab2/FlightPartitioner.java
+++ src/main/java/lab2/FlightPartitioner.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class FlightPartitioner extends Partitioner<AirportID, Text> {
+    public FlightPartitioner() { }
+
     public int getPartition(AirportID key, Text value, int numReduceTasks) {
         return ((key.value.hashCode() & Integer.MAX_VALUE)) % numReduceTasks;
     }
