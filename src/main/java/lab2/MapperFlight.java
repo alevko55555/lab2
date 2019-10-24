16fa216892dea2057da6d84e451f3d578584e870
alevko55555 <alevko55555@gmail.com>
2019-10-24 19:06:32 +0300

    Scripted auto-commit on change (2019-10-24 19:06:32) by gitwatch.sh

--- src/main/java/lab2/MapperFlight.java
+++ src/main/java/lab2/MapperFlight.java
@@ -2,7 +2,15 @@ package lab2;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 
-public class MapperFlight extends Mapper<LongWritable, Text, TextPair, Text> {
+import java.io.IOException;
+
+public class MapperFlight extends Mapper<LongWritable, Text, AirportID, Text> {
     @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if (key.get() == 0) {
+            return;
+        }
+    }
 }
