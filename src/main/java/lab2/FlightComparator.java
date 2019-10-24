29e5e22f438312a3486cb881d6d731b4c3f1d22d
alevko55555 <alevko55555@gmail.com>
2019-10-24 19:34:16 +0300

    Scripted auto-commit on change (2019-10-24 19:34:16) by gitwatch.sh

--- src/main/java/lab2/FlightComparator.java
+++ src/main/java/lab2/FlightComparator.java
@@ -4,6 +4,6 @@ import org.apache.hadoop.io.WritableComparator;
 
 public class FlightComparator  extends WritableComparator {
     public FlightComparator() {
-        
+        super (AirportID.class) ;
     }
 }
