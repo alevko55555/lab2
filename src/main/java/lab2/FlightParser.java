7f40b77ea1349605c6ec7cd3e4ddf0e8e8d6b144
alevko55555 <alevko55555@gmail.com>
2019-10-24 21:07:00 +0300

    Scripted auto-commit on change (2019-10-24 21:07:00) by gitwatch.sh

--- src/main/java/lab2/FlightParser.java
+++ src/main/java/lab2/FlightParser.java
@@ -4,4 +4,10 @@ public class FlightParser {
     public FlightParser(String str) {
 
     }
+    public String getDelayIdAirport() {
+        return();
+    }
+    public String getDelay() {
+        return();
+    }
 }
