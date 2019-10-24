e6321531eb370a595bdc1a2c7e64f638a05fffde
alevko55555 <alevko55555@gmail.com>
2019-10-24 20:40:29 +0300

    Scripted auto-commit on change (2019-10-24 20:40:29) by gitwatch.sh

--- src/main/java/lab2/JoinAirportFlight.java
+++ src/main/java/lab2/JoinAirportFlight.java
@@ -10,7 +10,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class JoinAirportFlight {
     public static void main(String args[]) throws Exception {
         if (args.length != 2) {
-            System.err.println("Usage: WordCountApp <input path> <output path>");
+            System.err.println("Usage: lab2.JoinAirportFlight <input path> <output path>");
             System.exit(-1);
         }
 
