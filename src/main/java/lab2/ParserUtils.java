751be8aa930536c0bc5654fa169f18b108272ab1
alevko55555 <alevko55555@gmail.com>
2019-10-24 21:42:56 +0300

    Scripted auto-commit on change (2019-10-24 21:42:56) by gitwatch.sh

--- src/main/java/lab2/ParserUtils.java
+++ src/main/java/lab2/ParserUtils.java
@@ -2,7 +2,7 @@ package lab2;
 
 public class ParserUtils {
     public static String[] getPart (String str) {
-        String[] part = str.split(",");
+        String[] part = str.split(",[\"]");
         for(int i = 0; i < part.length; i++) {
             part[i] = part[i].replaceAll("\"", "");
         }
