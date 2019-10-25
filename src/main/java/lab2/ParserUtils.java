4195d94d066da952bfc32f9a6641cd1bb2dd8771
alevko55555 <alevko55555@gmail.com>
2019-10-25 14:56:00 +0300

    Scripted auto-commit on change (2019-10-25 14:56:00) by gitwatch.sh

--- src/main/java/lab2/ParserUtils.java
+++ src/main/java/lab2/ParserUtils.java
@@ -2,17 +2,20 @@ package lab2;
 
 //import java.util.Optional;
 
+import java.util.ArrayList;
+
 public class ParserUtils {
-    private String[] part;
+    private ArrayList<String> part;
 
     ParserUtils(String str) {
-        part = str.split(",");
-        for(int i = 0; i < part.length; i++) {
-            part[i] = part[i].replaceAll("\"", "");
+        part = new ArrayList<>();
+        String[] part1 = str.split(",");
+        for(int i = 0; i < part1.length; i++) {
+            part.add(part1[i].replaceAll("\"", ""));
         }
     }
     String getString(int index) {
-        return part[index];
+        return part.get(index);
     }
 
 //    Optional<String> getString(int index) {
