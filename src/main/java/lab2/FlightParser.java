debd8e9232691f4b5e2d96bc0928cc0e43e31139
alevko55555 <alevko55555@gmail.com>
2019-10-26 10:36:24 +0300

    Scripted auto-commit on change (2019-10-26 10:36:24) by gitwatch.sh

--- src/main/java/lab2/FlightParser.java
+++ src/main/java/lab2/FlightParser.java
@@ -1,14 +1,17 @@
 package lab2;
 
 public class FlightParser extends ParserUtils{
+    private static final int DELAY_COL = 14;
+    private static final int DELAY_NEW_COL = 18;
+
     public FlightParser(String str) {
         super(str);
     }
     public String getDelayIdAirport() {
-        return getString(14);
+        return getString(DELAY_COL);
     }
     public String getDelay() {
-        String delay = getString(18);
+        String delay = getString(DELAY_NEW_COL);
         return delay.isEmpty() ? "0.0" : delay;
     }
 }
